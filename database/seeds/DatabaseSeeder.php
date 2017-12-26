df331e8f02092d7ffc31fe6252a8c513d794fb39
Glenn Plas <glenn@byte-consult.be>
2017-12-26 10:42:25 +0100

    adapt migrations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -21,12 +21,14 @@ class DatabaseSeeder extends Seeder
         5 "," Slave   | 2017-12-19 15:29:51 | 2017-12-19 15:29:55
         0 "," Default | 2017-12-19 15:29:51 | 2017-12-19 15:29:55
         */
+        DB::table('categories')->delete();
         $this->command->info('Seeding Categories');
         DB::table('categories')->insert(
-            [ 'name' => 'Default' ],
-            [ 'name' => 'Single' ],
-            [ 'name' => 'Master' ],
-            [ 'name' => 'Slave' ]
+            [ 'id'=>0, 'name' => 'Default' ],
+            [ 'id'=>1, 'name' => 'Single' ],
+            [ 'id'=>2, 'name' => 'Master' ],
+            [ 'id'=>3, 'name' => 'Slave' ],
+            [ 'id'=>4, 'name' => 'VIP' ]
         );
         $this->command->info('Seeding Bouncers');
         $this->call('BouncerTableSeeder');
@@ -40,20 +42,48 @@ class BouncerTableSeeder extends Seeder {
      * @return void
      */
     public function run() {
+/*
+ id |  name   | created_at | updated_at 
+----+---------+------------+------------
+  0 | Default |            | 
+  1 | Single  |            | 
+  2 | Master  |            | 
+  3 | Slave   |            | 
+  4 | VIP     |            | 
+
+ id          | integer                        | not null default nextval('bouncers_id_seq'::regclass)
+ label       | character varying(255)         | not null
+ category_id | integer                        | not null
+ dsn         | character varying(255)         | not null
+ priority    | integer                        | not null default 0
+ description | character varying(255)         | not null
+ created_at  | timestamp(0) without time zone | 
+ updated_at  | timestamp(0) without time zone | 
+ enabled     | boolean                        | not null default false
+ tag         | character varying(255)         | 
+ cluster_id  | integer                        | 
+ role        | character varying(255)         | 
+
+
+*/
         $bball = array(
-            array("label"=>"bouncer-fidus", "category_id"=>0,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.26.24:5432/pgbouncer","priority"=>0,"description"=>"Sudif bouncer", "enabled"=>true),
-            array("label"=>"bouncer-ppc-vip", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.34.134:5432/pgbouncer","priority"=>0 ,"description"=>"PPC test bouncer", "enabled"=>true),
-            array("label"=>"bouncer-arts", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.16.7:5432/pgbouncer","priority"=>0 ,"description"=>"Arts test bouncer", "enabled"=>true),
-            array("label"=>"bouncer-ppc-arts", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.14.231:5432/pgbouncer","priority"=>0 ,"description"=>"PPC arts bouncer", "enabled"=>true),
-            array("label"=>"bouncer-smu", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.16.8:5432/pgbouncer","priority"=>0 ,"description"=>"First test bouncer", "enabled"=>true),
-            array("label"=>"bouncer-ppc-smu", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.14.232:5432/pgbouncer","priority"=>0 ,"description"=>"PPC smu staging bouncer", "enabled"=>true),
-            array("label"=>"bouncer-ppc-smu-sta", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.22.232:5432/pgbouncer","priority"=>0 ,"description"=>"PPC smu staging bouncer", "enabled"=>true),
-            array("label"=>"bouncer-ppc-arts-sta", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.13.207:5432/pgbouncer","priority"=>0 ,"description"=>"PPC arts staging bouncer", "enabled"=>true),
-            array("label"=>"bouncer-arts-tst", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.23.56:5432/pgbouncer","priority"=>0 ,"description"=>"Arts test bouncer", "enabled"=>true),
-            array("label"=>"bouncer-smu-tst", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.23.57:5432/pgbouncer","priority"=>0 ,"description"=>"SMU test bouncer", "enabled"=>true),
-            array("label"=>"bouncer-smu-sta", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.23.58:5432/pgbouncer","priority"=>99 ,"description"=>"SMU sta bouncer", "enabled"=>true),
-            array("label"=>"bouncer-arts-sta", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.23.55:5432/pgbouncer","priority"=>99 ,"description"=>"ARTS sta bouncer", "enabled"=>true),
-            array("label"=>"bouncer-home", "category_id"=>0 ,"dsn"=>"postgresql://bouncermon:bouncerpass@127.0.0.1:5432/pgbouncer","priority"=>0 ,"description"=>"Home tester", "enabled"=>true) 
+            array("label"=>"bouncer-fidus-vip", "category_id"=>4,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.26.24:5432/pgbouncer","priority"=>0,"tag"=> "prd" ,"description"=>"Sudif bouncer", "enabled"=>true),
+            array("label"=>"bouncer-internal-vip", "category_id"=>4 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.34.134:5432/pgbouncer","priority"=>0 ,"tag"=> "internal","description"=>"PPC test bouncer", "enabled"=>true),
+            array("label"=>"bouncer-arts", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.16.7:5432/pgbouncer","priority"=>0 ,"tag"=> "prd","description"=>"Arts bouncer", "enabled"=>true),
+            array("label"=>"bouncer-ppc-arts", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.14.231:5432/pgbouncer","priority"=>0 ,"tag"=> "prd","description"=>"PPC ARTS prd", "enabled"=>true),
+            array("label"=>"bouncer-smu", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.16.8:5432/pgbouncer","priority"=>0 ,"tag"=> "prd","description"=>"SMU prd", "enabled"=>true),
+            array("label"=>"bouncer-ppc-smu", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.14.232:5432/pgbouncer","priority"=>0 ,"tag"=> "prd","description"=>"PPC SMU prd", "enabled"=>true),
+            array("label"=>"bouncer-ppc-smu-sta", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.22.232:5432/pgbouncer","priority"=>0 ,"tag"=> "sta","description"=>"PPC smu staging", "enabled"=>true),
+            array("label"=>"bouncer-ppc-arts-sta", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.13.207:5432/pgbouncer","priority"=>0 , "tag"=> "tst","description"=>"PPC arts tst", "enabled"=>true),
+            array("label"=>"bouncer-arts-tst", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.23.56:5432/pgbouncer","priority"=>0 ,"tag"=> "tst","description"=>"Arts test", "enabled"=>true),
+            array("label"=>"bouncer-smu-tst", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.23.57:5432/pgbouncer","priority"=>0 ,"tag"=> "tst","description"=>"SMU test", "enabled"=>true),
+            array("label"=>"bouncer-smu-sta", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.23.58:5432/pgbouncer","priority"=>99 ,"tag"=> "sta","description"=>"SMU sta", "enabled"=>true),
+            array("label"=>"bouncer-arts-sta", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.23.55:5432/pgbouncer","priority"=>99 ,"tag"=> "sta","description"=>"ARTS sta", "enabled"=>true),
+            array("label"=>"bouncer-home", "category_id"=>0 ,"dsn"=>"postgresql://bouncermon:bouncerpass@127.0.0.1:5432/pgbouncer","priority"=>0 ,"tag"=> "internal","description"=>"Home tester", "enabled"=>false),
+            array("label"=>"bouncer-arts-dev", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.26.157:5432/pgbouncer","priority"=>0 ,"tag"=> "dev","description"=>"Arts test", "enabled"=>true),
+            array("label"=>"bouncer-arts-dev", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.26.158:5432/pgbouncer","priority"=>0 ,"tag"=> "dev","description"=>"Siamu test", "enabled"=>true),
+            array("label"=>"bouncer-ppc-arts-sta", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.22.231:5432/pgbouncer","priority"=>0 ,"tag"=> "sta","description"=>"Arts PPC staging", "enabled"=>true),
+            array("label"=>"bouncer-ppc-arts-sta", "category_id"=>0 ,"dsn"=>"postgresql://pghud:OVBx0iyJIM6qE@192.168.22.231:5432/pgbouncer","priority"=>0 ,"tag"=> "sta","description"=>"Arts PPC staging", "enabled"=>true)
         );
 
         DB::table('bouncers')->delete();
